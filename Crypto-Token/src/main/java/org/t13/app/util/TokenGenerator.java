e79ca1340198b6621616a7c8cdaea8d2fe41da2b
thanish13 <thanishmalai13@gmail.com>
2025-09-19 19:56:27 +0530

    Encoding digestUtils

--- Crypto-Token/src/main/java/org/t13/app/util/TokenGenerator.java
+++ Crypto-Token/src/main/java/org/t13/app/util/TokenGenerator.java
@@ -1,11 +1,14 @@
 package org.t13.app.util;
 
+import org.springframework.util.DigestUtils;
+
 import java.util.Base64;
 import java.util.UUID;
 
 public class TokenGenerator {
     public static String generateToken(String input) {
-        return Base64.getEncoder().encodeToString((input + UUID.randomUUID()).getBytes()
+        return Base64.getEncoder().encodeToString(
+                DigestUtils.md5Digest((input + UUID.randomUUID()).getBytes())
         );
     }
 }
