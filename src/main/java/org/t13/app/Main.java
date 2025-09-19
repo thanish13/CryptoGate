e92b15ec263ccc13fc03ff78e107cf075c7e43a3
thanish13 <thanishmalai13@gmail.com>
2025-09-19 15:17:39 +0530

    Crypto Gate - Project Structure

--- src/main/java/org/t13/app/Main.java
+++ src/main/java/org/t13/app/Main.java
@@ -6,18 +6,15 @@ import org.springframework.context.ConfigurableApplicationContext;
 import java.util.ArrayList;
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+
+        System.out.print("Hello and welcome!");
 
         // Base Runner class (or interface)
         abstract class AbstractSpringBootRunner {
-            protected Class<?> applicationClass;
+            protected final Class<?> applicationClass;
 
             public AbstractSpringBootRunner(Class<?> applicationClass) {
                 this.applicationClass = applicationClass;
@@ -46,12 +43,6 @@ public class Main {
                     context.close();
                 }
             }
-        }
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
         }
 
         List<AbstractSpringBootRunner> runners = new ArrayList<>();
